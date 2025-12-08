766965f16267322d57aa6c8184faba9d063a678f
Anima3213 <109627078+GrumpyBud@users.noreply.github.com>
2025-12-08 17:15:38 -0500

    Refactor roller subsystem to implement closed-loop control and update command bindings for velocity settings

--- src/main/java/frc/robot/subsystems/roller/Roller.java
+++ src/main/java/frc/robot/subsystems/roller/Roller.java
@@ -1,14 +1,24 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.roller;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Pounds;
+import static edu.wpi.first.units.Units.RPM;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.units.measure.*;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import yams.gearing.GearBox;
 import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
 import yams.motorcontrollers.SmartMotorController;
 import yams.motorcontrollers.SmartMotorControllerConfig;
 import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
@@ -17,46 +27,115 @@ import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
 import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class Roller extends SubsystemBase {
-  private SmartMotorControllerConfig rollerConfig =
+
+  private SmartMotorControllerConfig smcConfig =
       new SmartMotorControllerConfig(this)
-          .withControlMode(ControlMode.OPEN_LOOP)
-          .withTelemetry("RollerMotor", TelemetryVerbosity.HIGH)
-          .withGearing(new MechanismGearing(GearBox.fromReductionStages(1)))
+          .withControlMode(ControlMode.CLOSED_LOOP)
+          // Feedback Constants (PID Constants)
+          .withClosedLoopController(1, 0, 0)
+          .withSimClosedLoopController(1, 0, 0)
+          // Feedforward Constants
+          .withFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+          .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0))
+          // Telemetry name and verbosity level
+          .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+          // Gearing from the motor rotor to final shaft.
+          // In this example GearBox.fromReductionStages(3,4) is the same as
+          // GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your
+          // motor.
+          // You could also use .withGearing(12) which does the same thing.
+          .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+          // Motor properties to prevent over currenting.
           .withMotorInverted(true)
-          .withIdleMode(MotorMode.BRAKE)
-          .withStatorCurrentLimit(Amps.of(40));
+          .withIdleMode(MotorMode.COAST)
+          .withStatorCurrentLimit(Amps.of(50));
 
-  private TalonFX rollerTalonFX = new TalonFX(14);
+  // Vendor motor controller object
+  private TalonFX spark = new TalonFX(14);
 
-  private SmartMotorController rollerController =
-      new TalonFXWrapper(rollerTalonFX, DCMotor.getFalcon500(1), rollerConfig);
+  // Create our SmartMotorController from our Spark and config with the NEO.
+  private SmartMotorController talonSmartMotorController =
+      new TalonFXWrapper(spark, DCMotor.getFalcon500(1), smcConfig);
 
+  private final FlyWheelConfig shooterConfig =
+      new FlyWheelConfig(talonSmartMotorController)
+          // Diameter of the flywheel.
+          .withDiameter(Inches.of(4))
+          // Mass of the flywheel.
+          .withMass(Pounds.of(1))
+          // Maximum speed of the shooter.
+          .withUpperSoftLimit(RPM.of(1000))
+          // Telemetry name and verbosity for the arm.
+          .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+
+  // Shooter Mechanism
+  private FlyWheel shooter = new FlyWheel(shooterConfig);
+
+  /**
+   * Gets the current velocity of the shooter.
+   *
+   * @return Shooter velocity.
+   */
   public AngularVelocity getVelocity() {
-    return rollerController.getMechanismVelocity();
+    return shooter.getSpeed();
   }
 
-  public void setDutyCycle(double dutyCycle) {
-    rollerController.setDutyCycle(dutyCycle);
+  /**
+   * Set the shooter velocity.
+   *
+   * @param speed Speed to set.
+   * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
+   */
+  public Command setVelocity(AngularVelocity speed) {
+    return shooter.setSpeed(speed);
   }
 
+  /**
+   * Set the dutycycle of the shooter.
+   *
+   * @param dutyCycle DutyCycle to set.
+   * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
+   */
   public Command set(double dutyCycle) {
-    return this.run(() -> this.setDutyCycle(dutyCycle))
-        .finallyDo(
-            interrupted -> {
-              if (interrupted) {
-                this.setDutyCycle(0);
-              }
-            })
-        .withName("SetRoller");
+    return shooter.set(dutyCycle);
+  }
+
+  /** Creates a new ExampleSubsystem. */
+  public Roller() {}
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
   }
 
   @Override
   public void periodic() {
-    rollerController.updateTelemetry();
+    // This method will be called once per scheduler run
+    shooter.updateTelemetry();
   }
 
   @Override
   public void simulationPeriodic() {
-    rollerController.simIterate();
+    // This method will be called once per scheduler run during simulation
+    shooter.simIterate();
   }
 }
